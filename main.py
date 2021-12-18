ee541392ee6f523cec3cd2f051dd57c02059cfcc
Wen Wei Tseng <sosiristseng@gmail.com>
2021-12-18 13:38:48 +0000

    Add 2D glucose data. close #5

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-##############################################################################################
-## 0X code
-
 import networkmodel
 import myGA
 import KLDivergence
@@ -131,11 +128,10 @@ def run(
 
 if __name__ == "__main__":
 
-    # create true answer from image analysis
-    # run("data/2D_glucose/0X_fitting.csv")
-    # run("data/2D_glucose/1X_fitting.csv")
-    # run("data/2D_glucose/3X_fitting.csv")
-    # run("data/2D_glucose/6X_fitting.csv")
+    run("data/0X_fitting.csv")
+    run("data/1X_fitting.csv")
+    run("data/3X_fitting.csv")
+    run("data/6X_fitting.csv")
 
     gaargs = {
         "max_num_iteration": 20,
